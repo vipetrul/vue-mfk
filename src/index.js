9273c6c1de653d2dda5907c1cb1748ef67498a4a
Unknown <vipetrul@gmail.com>
2017-12-31 12:36:47 -0600

    Initial commit

---
+++ src/index.js
@@ -0,0 +1,32 @@
+//needed for Vuetify
+require ('vuetify/src/stylus/app.styl')
+import Vuetify from 'vuetify/es5/components/Vuetify'
+import VApp from 'vuetify/es5/components/VApp'
+import VTextField from 'vuetify/es5/components/VTextField'
+
+import Hello from './Hello.vue'
+import MfkInput from './mfk-input.vue'
+
+function plugin(Vue) {
+  
+  //Use folling components from vuetify
+  Vue.use(Vuetify, { components: { VApp, VTextField } })
+  
+  Vue.component('hello', Hello)
+  Vue.component('mfk-input', MfkInput)
+}
+
+// Install by default if using the script tag
+if (typeof window !== 'undefined' && window.Vue) {
+  window.Vue.use(plugin)
+}
+
+export default plugin
+const version = '__VERSION__'
+// Export all components too
+export {
+  VApp,
+  VTextField,
+  Hello,
+  version
+}
---
+++ src/mfk-input.vue
@@ -0,0 +1,17 @@
+<template>
+  <v-text-field
+              name="input-1"
+              label="Label Text"
+              id="testing"
+            ></v-text-field>
+</template>
+
+<script>
+import VTextField from 'vuetify/es5/components/VTextField'
+
+export default {};
+</script>
+
+<style>
+
+</style>
