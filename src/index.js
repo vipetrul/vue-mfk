337e718dda80710fea2e270b30ccf2632132e705
Unknown <vipetrul@gmail.com>
2018-01-14 15:22:25 -0600

    Removed Hello component

--- src/index.js
+++ src/index.js
@@ -1,6 +1,5 @@
 import Vuetify from 'vuetify'
 
-import Hello from './Hello.vue'
 import MfkInput from './mfk-input.vue'
 import MfkFavotire from './mfk-favorite.vue'
 
@@ -10,7 +9,6 @@ function plugin(Vue) {
   //Use folling components from vuetify
   Vue.use(Vuetify)
   
-  Vue.component('hello', Hello)
   Vue.component('mfk-input', MfkInput)
   Vue.component('mfk-favorite', MfkFavotire)
 }
@@ -26,6 +24,5 @@ const version = '__VERSION__'
 export {
   MfkInput,
   MfkFavotire,
-  Hello,
   version
 }
